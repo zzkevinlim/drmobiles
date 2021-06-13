e1d83ca39960c9464a5a88713237178d65899144
Kevin Lim <zzkevinlim@gmail.com>
2021-06-13 23:12:45 +1200

    202106132312

--- htdocs/content/themes/drmobiles/views/layouts/main.blade.php
+++ htdocs/content/themes/drmobiles/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="{{ get_bloginfo('charset') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="S6UYzFUQ-I7RdrG_4L1-mlGubitZ0ua2iixgngtK6Kw" />
     <link rel="profile" href="https://gmpg.org/xfn/11">
     @head
 </head>
